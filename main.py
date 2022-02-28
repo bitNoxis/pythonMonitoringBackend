d37eff2d9c04b3e945e52895ff9f5bad21499f47
bitNoxis <78650305+bitNoxis@users.noreply.github.com>
2022-02-28 10:46:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,80 @@
 import mysql.connector
+import time
+import socket
 
-mydb = mysql.connector.connect(
-    host="34.159.157.247",
-    user="root",
-    passwd="OA6I2K76r7BzEIGC",
-    database="datenbank"
-)
+table = "entries"
 
-print(mydb)
+def main():
+    if isConnectionOk():
+        datenbankAnfrage()
+    while True:
+        if isConnectionOk():
+            insertData(str(get_cpu_temp()), str(get_cpu_speed()))
+       
+        waitFor(30)
 
-mycursor = mydb.cursor()
+def datenbankAnfrage():
+    try:
+        mydb = mysql.connector.connect(
+            host="34.159.157.247",
+            user="root",
+            passwd="OA6I2K76r7BzEIGC",
+            database="monitoringDatenbank"
+            )
 
-mycursor.execute("SELECT * FROM entries")
-myresult = mycursor.fetchall()
-for x in myresult:
-    print(x)
+        print(mydb)
+
+        mycursor = mydb.cursor()
+
+        mycursor.execute("SELECT * FROM " + table)
+        myresult = mycursor.fetchall()
+        for x in myresult:
+            print(x)  
+    except:
+        print("Datenbank Anfrage Error")
+def get_cpu_temp():
+        tempFile = open("/sys/class/thermal/thermal_zone0/temp")
+        cpu_temp = tempFile.read()
+        tempFile.close()
+        return round(float(cpu_temp)/1000, 1)
+
+def get_cpu_speed():
+        tempFile = open("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq")
+        cpu_speed = tempFile.read()
+        tempFile.close()
+        return float(cpu_speed)/1000000
+   
+def insertData(temp, speed):
+    try:
+        mydb = mysql.connector.connect(
+        host="34.159.157.247",
+        user="root",
+        passwd="OA6I2K76r7BzEIGC",
+        database="monitoringDatenbank"
+        )
+
+        print(mydb)
+
+        mycursor = mydb.cursor()
+        print("Try insert: " + temp + " " + speed)
+        mycursor.execute("INSERT INTO " + table + "(cpuTemp, cpuSpeed) VALUES (" + temp + ", " + speed + ");")
+        print("insert complete")
+        mydb.commit()
+        datenbankAnfrage()
+    except:
+        print("Daten insert Error")
+       
+def isConnectionOk():
+    try:
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except:
+        print('Keine Internetverbindung')
+        return False
+   
+def waitFor(timer):
+    for x in range(timer):
+        time.sleep(1)
+        print(x)
+       
+main()
